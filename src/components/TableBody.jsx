e3bcd10deee7008c87b2dafd15adaa148787bf3c
Aji Hanggoro <adjihans@gmail.com>
2020-10-20 23:24:22 +0700

    add components: 1. TableBody --> for show body
    add Pages:
    1. Form Login --> kosongan
    2. Form Register --> kosongan
    3. Form Question --> syudah
    4. Form Add Sample --> syudah
    5. Form Add Solution --> syudah
    6. Form Room --> syudah
    7. Rooms --> buat nampilin list rooms
    8. Questions --> buat nampilin list question
    9. Schedule --> opsional buat upcoming kalo jadi

---
+++ src/components/TableBody.jsx
@@ -0,0 +1,174 @@
+import React from "react"
+import { Box, Button, List, ListItem, Flex, Text } from "@chakra-ui/core"
+import { useHistory } from "react-router-dom"
+
+export default function TableBody(props) {
+    const history = useHistory()
+
+    function updateOnClick(event) {
+        event.preventDefault()
+        console.log("update")
+    }
+
+    function deleteOnClick(event) {
+        event.preventDefault()
+        console.log("delete")
+    }
+
+    function solutionOnClick(event) {
+        event.preventDefault()
+        console.log(props.question._id)
+        history.push("/add-solution/" + props.question._id)
+    }
+
+    function sampleOnClick(event) {
+        event.preventDefault()
+        console.log(props.question._id)
+        history.push("/add-solution/" + props.question._id)
+    }
+    return (
+        <Flex
+            width="60%"
+            borderWidth="1px"
+            rounded="lg"
+            overflow="hidden"
+            alignItems="center"
+            p="4"
+            m="2"
+        >
+            {props.question && (
+                <>
+                    <Flex flex="4" direction="column">
+                        <Box
+                            color="gray.200"
+                            fontWeight="bold"
+                            letterSpacing="wide"
+                            textTransform="uppercase"
+                            ml="2"
+                        >
+                            <Text>{props.question.title}</Text>
+                        </Box>
+                        <Box color="gray.500" fontWeight="semibold" ml="2">
+                            <Text>{props.question.description}</Text>
+                        </Box>
+                        <Box color="gray.500" fontWeight="semibold" ml="2">
+                            <Text>Max Score: {props.question.score}</Text>
+                        </Box>
+                        <Box color="gray.500" fontWeight="semibold" ml="2">
+                            <Text>
+                                Time Limit: {props.question.timeLimit} minutes
+                            </Text>
+                        </Box>
+                    </Flex>
+                    <Flex
+                        flex="1"
+                        direction="column"
+                        alignItems="end"
+                        justify="end"
+                    >
+                        <Flex>
+                            <Flex
+                                flex="1"
+                                direction="column"
+                                align="center"
+                                mr="1"
+                            >
+                                <Button
+                                    color="gray.800"
+                                    leftIcon="edit"
+                                    variantColor="green"
+                                    m="2"
+                                    width="100%"
+                                    onClick={solutionOnClick}
+                                >
+                                    Add Solution
+                                </Button>
+                                <Button
+                                    color="gray.800"
+                                    leftIcon="edit"
+                                    variantColor="teal"
+                                    m="2"
+                                    width="100%"
+                                    onClick={sampleOnClick}
+                                >
+                                    Add Sample
+                                </Button>
+                            </Flex>
+                            <Flex flex="1" direction="column" align="start">
+                                <Button
+                                    color="gray.800"
+                                    leftIcon="edit"
+                                    variantColor="yellow"
+                                    m="2"
+                                    width="80%"
+                                    onClick={updateOnClick}
+                                >
+                                    Update
+                                </Button>
+
+                                <Button
+                                    color="gray.800"
+                                    leftIcon="delete"
+                                    variantColor="red"
+                                    m="2"
+                                    width="80%"
+                                    onClick={deleteOnClick}
+                                >
+                                    Delete
+                                </Button>
+                            </Flex>
+                        </Flex>
+                    </Flex>
+                </>
+            )}
+            {props.room && (
+                <>
+                    <Flex flex="4" direction="column">
+                        <Box
+                            color="gray.200"
+                            fontWeight="bold"
+                            letterSpacing="wide"
+                            textTransform="uppercase"
+                            ml="2"
+                        >
+                            <Text>Room {props.room.title} Details</Text>
+                        </Box>
+                        <Box color="gray.500" fontWeight="semibold" ml="2">
+                            <Text>Started at: {props.room.time_start}</Text>
+                        </Box>
+                        <Box color="gray.500" fontWeight="semibold" ml="2">
+                            <Text>Finish at: {props.room.time_end}</Text>
+                        </Box>
+                        <Box color="gray.500" fontWeight="semibold" ml="2">
+                            <List as="ol" styleType="decimal">
+                                <Text>Question Sets:</Text>
+                                {props.room.ids.map((question) => {
+                                    return (
+                                        <ListItem
+                                            key={question.id}
+                                            color="gray.600"
+                                        >
+                                            {question.title}
+                                        </ListItem>
+                                    )
+                                })}
+                            </List>
+                        </Box>
+                    </Flex>
+                    <Flex flex="1" direction="column" align="end">
+                        <Button
+                            color="gray.800"
+                            leftIcon="delete"
+                            variantColor="red"
+                            m="2"
+                            width="70%"
+                            onClick={deleteOnClick}
+                        >
+                            Delete
+                        </Button>
+                    </Flex>
+                </>
+            )}
+        </Flex>
+    )
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,5 +2,26 @@ import HomePage from "./HomePage"
 import RoomPage from "./RoomPage"
 import Dashboard from "./Dashboard"
 import DummyPage from "./DummyPage"
+import FormQuestion from "./FormQuestion"
+import FormLogin from "./FormLogin"
+import FormRegister from "./FormRegister"
+import Questions from "./Questions"
+import Rooms from "./Rooms"
+import FormRoom from "./FormRoom"
+import FormSampleSolution from "./FormSampleSolution"
+import FormSolution from "./FormSolution"
 
-export { HomePage, DummyPage, Dashboard, RoomPage }
+export {
+    HomePage,
+    DummyPage,
+    Dashboard,
+    RoomPage,
+    FormQuestion,
+    FormLogin,
+    FormRegister,
+    Questions,
+    Rooms,
+    FormRoom,
+    FormSampleSolution,
+    FormSolution,
+}
